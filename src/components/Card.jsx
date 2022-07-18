c20243b0ec6e02e012b829c05bdeb4399179b226
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-07-18 11:23:21 +0300

    code correction

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Card.css'
 
-
 export default (props) => {
+
+    const handleClick = () => props.onClick(props.id);
+
     return (
 
-        <div onClick={() => props.onClick(props.id)} className={"tarifCard " + (props.isChecked ? "checked" : "")} >
+
+        < div onClick={handleClick} className={"tarifCard " + (props.isChecked ? "checked" : "")} >
             <div className="cardName" style={{ backgroundColor: props.color }}>{props.name}</div>
             <div className="cardPrice" style={{ backgroundColor: props.color }}><span className="rub">руб </span>{props.price}<span className="mes"> /мес</span></div>
             <div className="cardMbit">{props.mbit}</div>
